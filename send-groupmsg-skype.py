ebd6ff941c9416a806c573b7cd480974bb52e221
Gary Hooi <38656260+garyhooi@users.noreply.github.com>
2020-10-14 19:03:35 +0800

    Update send-groupmsg-skype.py
    Added some code to skip Saturday and Sunday.

--- send-groupmsg-skype.py
+++ send-groupmsg-skype.py
@@ -38,8 +38,11 @@ if currentDate == endDate or currentDate > endDate:
     print(str(datetime.now().replace(microsecond=0)) + ": Thread expired, exiting...")
     thread.exit()
 else:
-    schedule.every().day.at(checkInTime).do(post_checkin, msgCheckIn, channel_id)
-    schedule.every().day.at(checkOutTime).do(post_checkout, msgCheckOut, channel_id)
+    if(int(currentDate.today().weekday()) == 5 or int(currentDate.today().weekday()) == 6):
+        print(str(datetime.now().replace(microsecond=0)) + ": Program skipped... Today is weekend.")
+    else:
+        schedule.every().day.at(checkInTime).do(post_checkin, msgCheckIn, channel_id)
+        schedule.every().day.at(checkOutTime).do(post_checkout, msgCheckOut, channel_id)
 
     while True:
         schedule.run_pending()
