cacae47403aea33e37e95c24373dccd9c483c97b
Gary Hooi <38656260+garyhooi@users.noreply.github.com>
2020-10-14 18:01:26 +0800

    Delete get-skype Group-id.py

--- get-skype Group-id.py
+++
@@ -1,7 +0,0 @@
-from skpy import Skype
-
-credentialId = ""                       # Skype Email
-credentialPwd = ""                      # Skype Password (For safety, use app password)
-
-skype_obj = Skype(credentialId, credentialPwd)
-skype_obj.chats.recent()                # Return Skype group id
